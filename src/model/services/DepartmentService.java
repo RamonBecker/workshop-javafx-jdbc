c4aa9a759caf75433906bd4e8013c4b9439c62b8
Ramon Becker <44611131+RamonBecker@users.noreply.github.com>
2021-09-27 19:29:08 -0300

    Deletando departamento

--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -16,10 +16,14 @@ public class DepartmentService {
 
 	public void saveOrUpdate(Department obj) {
 		if (obj.getId() == null) {
-		
+
 			dao.insert(obj);
 		} else {
 			dao.update(obj);
 		}
+	}
+
+	public void remove(Department obj) {
+		dao.deleteById(obj.getId());
 	}
 }
